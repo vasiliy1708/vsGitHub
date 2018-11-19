4d675f4ddc305608816d547af4a093af8363990b
Vasiliy <vasiliy1708@yandex.ru>
2018-11-19 16:47:33 +0400

    add MagicValue

--- vsGitHub/Program.cs
+++ vsGitHub/Program.cs
@@ -6,11 +6,42 @@ using System.Threading.Tasks;
 
 namespace vsGitHub
 {
+    class MagicValue
+    {
+        public int Left { get; set; }
+        public int Right { get; set; }
+
+        public MagicValue(int left, int right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public static void Apply(MagicValue magicValue)
+        {
+            magicValue.Left += 3;
+            magicValue.Right += 4;
+            magicValue = new MagicValue(5, 6);
+        }
+
+        public static void ApplyRef(ref MagicValue magicValue)
+        {
+            magicValue.Left += 7;
+            magicValue.Right += 8;
+            magicValue = new MagicValue(9, 10);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fifi");
+            var magicValue = new MagicValue(1, 2);
+            MagicValue.ApplyRef(ref magicValue);
+            MagicValue.Apply(magicValue);
+
+            Console.WriteLine(magicValue.Left * magicValue.Right);
+            Console.ReadKey();
         }
     }
 }
